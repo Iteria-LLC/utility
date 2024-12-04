d9ae935ea976f119fc8dd9ce38a6f4294c3f9365
Pete Goodfellow <petethegoat@gmail.com>
2024-12-04 01:28:39 +0000

    Update SquareDistance.cs with better "constants"

--- SquareDistance.cs
+++ SquareDistance.cs
@@ -6,6 +6,12 @@ namespace Iteria
 #endif
 	public static class Square
 	{
+		public static readonly SqrDist Zero = new(0);
+		public static readonly SqrDist MaxValue = new(float.MaxValue);
+		public static readonly SqrDist MinValue = new(float.MinValue);
+		public static readonly SqrDist Infinity = new(float.PositiveInfinity);
+		public static readonly SqrDist NegativeInfinity = new(float.NegativeInfinity);
+
 		public static SqrDist Distance(Vector3 a, Vector3 b)
 		{
 			return new SqrDist((a - b).sqrMagnitude, true);
@@ -16,8 +22,6 @@ namespace Iteria
 			public float Root => _root.GetValueOrDefault(Mathf.Sqrt(square));
 			readonly float? _root;
 			public readonly float square;
-
-			public static readonly SqrDist Infinity = new SqrDist(Mathf.Infinity);
 
 			public static bool operator >(SqrDist d, float a) => d.square > a * a;
 			public static bool operator <(SqrDist d, float a) => d.square < a * a;
